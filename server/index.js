a98fbfb2730e7c5819802392b9f0ed2709e0762a
sonmodx <sonmodx@gmail.com>
2023-11-30 01:25:42 +0700

    Add cors

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+
+app.use(cors());
 
 app.listen("3001", () => {
   console.log("Server is running on port 3001");
